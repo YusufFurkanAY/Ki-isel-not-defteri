e246a44d1f182ef1ec03d3adc582c73c2f33342d
YusufFurkanAY <139172208+YusufFurkanAY@users.noreply.github.com>
2023-07-10 23:00:12 +0300

    Kişisel not defteri

---
+++ Yeni klasör/YusufFurkanAY.cs
@@ -0,0 +1,22 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
